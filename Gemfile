bf5b251f7111734dcb894f709e493c41889da6a0
Albert Pak <al@albert-pak.com>
2016-03-01 11:01:15 -0600

    code cleanup, and set up prerending of React components for SEO

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'react-rails', '~> 1.6.0'
 
 group :development, :test do
   gem 'byebug'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'meta_request'
 end
 
 group :development do
---
+++ app/assets/javascripts/components/main.jsx
@@ -0,0 +1,13 @@
+class Main extends React.Component {
+  render() {
+    return (
+      <h1>Hello from SEO</h1>
+    );
+  }
+}
+
+// let documentReady = () => {
+//   ReactDOM.render(<Main />, document.getElementById('react'));
+// };
+//
+// $(documentReady);
