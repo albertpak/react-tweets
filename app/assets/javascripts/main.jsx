80d58ef7a5f602651558d4a3ac8b869fc76367aa
Albert Pak <al@albert-pak.com>
2016-03-01 10:43:23 -0600

    refactor react component into a class

--- app/assets/javascripts/main.jsx
+++ app/assets/javascripts/main.jsx
@@ -1,5 +1,13 @@
+class Main extends React.Component {
+  render() {
+    return (
+      <h1>Hello from component</h1>
+    );
+  }
+}
+
 let documentReady = () => {
-  ReactDOM.render(<h1>Hello</h1>, document.getElementById('react'));
+  ReactDOM.render(<Main />, document.getElementById('react'));
 };
 
 $(documentReady);
